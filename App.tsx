05202eed62f45ba84c4cb80b460baf09ce178d9f
murilo <muriraubach@gmail.com>
2022-01-02 21:09:16 -0300

    feat: add user identification page design

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 import AppLoading from 'expo-app-loading';
 
-import Welcome from './src/pages/Welcome';
+import UserIdentification from './src/pages/UserIdentification';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -14,6 +14,6 @@ export default function App() {
     return <AppLoading />;
   }
 
-  return <Welcome />;
+  return <UserIdentification />;
 }
 
