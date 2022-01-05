00d08a6c473a2644cfb8c99693e710cec6f66b49
murilo <muriraubach@gmail.com>
2022-01-04 23:49:52 -0300

    feat: add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import avatarImg from '../assets/avatar.png';
+
+import fonts from '../styles/fonts';
+import colors from '../styles/colors';
+
+const Header: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Murilo</Text>
+      </View>
+      <Image source={avatarImg} style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  userName: {
+    fontSize: 32,
+    lineHeight: 36,
+    color: colors.heading,
+    fontFamily: fonts.heading
+  },
+  image: {
+    width: 64,
+    height: 64,
+    borderRadius: 56,
+  }
+})
+
+export default Header;
