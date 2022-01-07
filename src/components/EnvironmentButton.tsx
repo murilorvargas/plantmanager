0a62eba9d567c4c835d7a0eb1609087308981b02
murilo <muriraubach@gmail.com>
2022-01-06 23:00:19 -0300

    feat: change environment button component to touchable opacity

--- src/components/EnvironmentButton.tsx
+++ src/components/EnvironmentButton.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
+import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
 
 import fonts from '../styles/fonts';
 import colors from '../styles/colors';
 
-interface EnvironmentButtonProps extends RectButtonProps {
+interface EnvironmentButtonProps extends TouchableOpacityProps {
   title: string;
   active?: boolean;
 }
 
 const EnvironmentButton: React.FC<EnvironmentButtonProps> = ({ title, active = false, ...rest }) => {
   return (
-    <RectButton style={[styles.container, active && styles.containerActive]} {...rest}>
+    <TouchableOpacity style={[styles.container, active && styles.containerActive]} {...rest}>
       <Text style={[styles.text, active && styles.textActive]}>{title}</Text>
-    </RectButton>
+    </TouchableOpacity>
   );
 }
 
