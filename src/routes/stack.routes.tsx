4d294f0ae85e9e6125524b37280e39cf82b33c96
murilo <muriraubach@gmail.com>
2022-01-07 21:00:41 -0300

    feat: add plant save page to routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import Welcome from '../pages/Welcome';
 import UserIdentification from '../pages/UserIdentification';
 import Confirmation from '../pages/Confirmation';
 import PlantSelect from '../pages/PlantSelect';
+import PlantSave from '../pages/PlantSave';
 
 const Stack = createNativeStackNavigator();
 
@@ -29,6 +30,10 @@ const AppRoutes: React.FC = () => (
     <Stack.Screen
       name="PlantSelect"
       component={PlantSelect}
+    />
+    <Stack.Screen
+      name="PlantSave"
+      component={PlantSave}
     />
   </Stack.Navigator>
 )
