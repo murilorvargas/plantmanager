bf3394be79593865034af01502ffb58dd799ce0a
murilo <muriraubach@gmail.com>
2022-01-09 00:08:42 -0300

    feat: add swipeable to delete plant in plant card secondary component

--- src/components/PlantCardSecondary.tsx
+++ src/components/PlantCardSecondary.tsx
@@ -1,28 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import { Animated, StyleSheet, Text, View } from 'react-native';
 import { SvgFromUri } from 'react-native-svg';
+import { Feather } from '@expo/vector-icons';
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 
 import fonts from '../styles/fonts';
 import colors from '../styles/colors';
+import { GestureHandlerRootView, RectButton, RectButtonProps } from 'react-native-gesture-handler';
 
-interface PlantCardPrimaryProps extends TouchableOpacityProps {
+interface PlantCardPrimaryProps extends RectButtonProps {
   data: {
     name: string;
     photo: string;
     hour: string;
-  }
+  };
+  handleRemove: () => void;
 }
 
-const PlantCardSecondary: React.FC<PlantCardPrimaryProps> = ({ data, ...rest }) => {
+const PlantCardSecondary: React.FC<PlantCardPrimaryProps> = ({ data, handleRemove, ...rest }) => {
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
-      <SvgFromUri uri={data.photo} width={50} height={50} />
-      <Text style={styles.title}>{data.name}</Text>
-      <View style={styles.details}>
-        <Text style={styles.timeLabel}>Regar às</Text>
-        <Text style={styles.time}>{data.hour}</Text>
-      </View>
-    </TouchableOpacity>
+    <GestureHandlerRootView>
+      <Swipeable overshootRight={false} renderRightActions={() => (
+        <Animated.View>
+          <View>
+            <RectButton style={styles.buttonRemove} onPress={handleRemove}>
+              <Feather name="trash" size={32} color={colors.white} />
+            </RectButton>
+          </View>
+        </Animated.View>
+      )}>
+        <RectButton style={styles.container} {...rest}>
+          <SvgFromUri uri={data.photo} width={50} height={50} />
+          <Text style={styles.title}>{data.name}</Text>
+          <View style={styles.details}>
+            <Text style={styles.timeLabel}>Regar às</Text>
+            <Text style={styles.time}>{data.hour}</Text>
+          </View>
+        </RectButton>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 }
 
@@ -57,6 +73,18 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: fonts.heading,
     color: colors.body_dark,
+  },
+  buttonRemove: {
+    width: 100,
+    height: 85,
+    backgroundColor: colors.red,
+    marginTop: 15,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    right: 20,
+    paddingLeft: 15,
   }
 })
 
